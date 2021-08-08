9a9aab844378a2c90fad1f80c70f8e095af8e8ac
Elias Wanner <62879938+ejwanner@users.noreply.github.com>
2021-08-08 21:06:08 +0200

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization');
   if (!authHeader) {
+    console.log('test');
     req.isAuth = false;
     return next();
   }
